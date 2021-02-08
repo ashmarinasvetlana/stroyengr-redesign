9eb7e38d77890f850cdf0daaa9ffa650d03d5098
Svetlana Ashmarina <sionova2@gmail.com>
2021-02-08 08:37:17 +0300

    Menu fix

--- js/modules/burger.js
+++ js/modules/burger.js
@@ -3,7 +3,9 @@ window.addEventListener('DOMContentLoaded', function () {
 
     const burgerBtn = document.querySelector('.header-burger'),
     mobileMenu = document.querySelector('.header-nav__mobile'),
+    menuItems = document.querySelectorAll('.menu-mobile'),
     mobileMenuClose = document.querySelector('.mobile-menu_close');
+    console.log(menuItems);
  
 
 burgerBtn.addEventListener('click', function () {
@@ -18,5 +20,14 @@ mobileMenuClose.addEventListener('click', function () {
 
 });
 
-});
+mobileMenu.addEventListener('click', function(event) {
+    let target = event.target;
+    if (target && target.classList.contains('menu-mobile')) {
 
+                mobileMenu.classList.remove('menu-animate');
+
+
+    }
+
+});
+});
